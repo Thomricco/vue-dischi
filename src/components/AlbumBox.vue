92bc135266d3a021f8b4063b06c3450f76f688a4
Thomricco <tommasoricco@hotmail.it>
2022-01-31 16:53:17 +0100

    fine. no bonus

---
+++ src/components/AlbumBox.vue
@@ -0,0 +1,53 @@
+<template>
+    
+    <div class="album">
+        <img :src="albums.poster" class="box-img" alt="">
+        <p class="text-title">{{albums.title}}</p>
+        <p class="text-author">{{albums.author}}</p>
+        <p class="text-years">{{albums.year}}</p>
+    </div>
+        
+    
+</template>
+
+
+<script>
+export default {
+    props: {
+        albums: Object
+    }
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+.album {
+    width: 150px;
+    height: 300px;
+    text-align: center;
+    background-color: #2E3A46;
+    margin: 10px;
+    padding: 10px;
+    padding-top: 20px;
+    .text-title {
+        color: #fff;
+        font-size: 20px;
+        padding-bottom: 10px;
+        text-transform:uppercase;
+    }
+    .text-author {
+        color: gray;
+        font-size: 15px;
+    }
+    .text-years {
+        color: gray;
+        font-size: 12px;
+    }
+    .box-img {
+        width: 120px;
+        height: 120px;
+        margin-bottom: 10px;
+    }
+}
+</style>
---
+++ src/components/MainContainer.vue
@@ -0,0 +1,60 @@
+<template>
+    <header>
+        <div class="nav">
+            <img class="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Spotify_App_Logo.svg/2048px-Spotify_App_Logo.svg.png" alt="">
+        </div>
+        <div class="container">
+            <div class="containar-album">
+                <album-box v-for="(albums, index) in album" 
+                :key=(index)
+                :albums="albums" />
+            </div>
+        </div>
+        
+    </header>
+</template>
+
+
+<script>
+import AlbumBox from './AlbumBox.vue'
+export default {
+    components: {
+        AlbumBox,
+    },
+    props: {
+        album: Array
+    }
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    header {
+    background-color: #1E2D3B;
+        .nav {
+                height: 50px;
+                background-color: black;
+                .logo {
+                    width: 50px;
+                }
+            }
+        .container {
+            width: 1200px;
+            margin: 0 auto;
+            
+            .containar-album {
+                margin: 0 auto;
+                width: 1000px;
+                padding-top: 50px;
+                padding-bottom: 50px;
+                margin: 0 auto;
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: center;
+                
+            }
+            }
+
+
+    }
+</style>
